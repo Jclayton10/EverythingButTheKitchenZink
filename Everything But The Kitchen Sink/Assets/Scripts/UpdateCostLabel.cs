d73c104c66a2bcfed9c9fd1e7d0bd107a818441f
Jack Stormont <stormont.jack@gmail.com>
2022-11-11 23:45:05 -0500

    Fixed 3 Level Room Top Door and also improved the UpdateCostLabel Script

--- Everything But The Kitchen Sink/Assets/Scripts/UpdateCostLabel.cs
+++ Everything But The Kitchen Sink/Assets/Scripts/UpdateCostLabel.cs
@@ -1,12 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
 public class UpdateCostLabel : MonoBehaviour
 {
-    private void Update()
+    private int currentCost, prevCost = -1;
+    public GameObject GlobalStats;
+
+	private void Awake()
+	{
+        GlobalStats = GameObject.Find("Global Stats");
+
+    }
+	private void Update()
     {
-        GetComponent<TextMeshPro>().text = GameObject.Find("Global Stats").GetComponent<GlobalStats>().costForRoom + " Gold";
+        currentCost = GlobalStats.GetComponent<GlobalStats>().costForRoom;
+        if (currentCost != prevCost)
+		{
+            GetComponent<TextMeshPro>().text = currentCost + " Gold";
+            prevCost = currentCost;
+        }
     }
 }
