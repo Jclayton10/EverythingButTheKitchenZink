a3667424e726f8c35ec7b63b13f6571a07fb02ae
Jack Stormont <stormont.jack@gmail.com>
2022-11-16 15:02:35 -0500

    Zombert Fashion Update
    Changed the color generation to pick off the texture, not create a new material per texture. Also, added 3 new shirt colors, bumping up the amount of options of spawnable Zomberts to 18.

--- Everything But The Kitchen Sink/Assets/Scripts/ZombertSkinChange.cs
+++ Everything But The Kitchen Sink/Assets/Scripts/ZombertSkinChange.cs
@@ -3,13 +3,13 @@ using UnityEngine;
 
 public class ZombertSkinChange : MonoBehaviour
 {
-    public List<Material> zombertSkins;
-    public Material selectedMaterial;
+    public List<Texture> zombertSkins;
+    public Texture selectedTexture;
     void Start()
     {
         int selectedIndex = Mathf.FloorToInt(Random.Range(0f, (float)zombertSkins.Count));
-        selectedMaterial = zombertSkins[selectedIndex];
-        gameObject.GetComponent<Renderer>().material = selectedMaterial;
+        selectedTexture = zombertSkins[selectedIndex];
+        gameObject.GetComponent<Renderer>().material.SetTexture("_MainTex", selectedTexture);
     }
 
     
